17b65054aa66b90b17c5e952b210edb759d9d11d
gudnex <marcelgoodness144@gmail.com>
2026-02-28 22:33:43 +0100

    fix: clean up formatting and ensure consistency in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,24 +2,40 @@
 const nextConfig = {
   reactCompiler: true,
 
-  serverExternalPackages: ["pg", "sequelize" , "pg-hstore", "dotenv"],
+  serverExternalPackages: ["pg", "sequelize", "pg-hstore", "dotenv"],
 
   onDemandEntries: {
     maxInactiveAge: 60 * 60 * 1000, // 1 hour
     pagesBufferLength: 5,
   },
 
-   experimental: {
+  experimental: {
     turbo: {
       rules: {
-        '*.svg': ['@svgr/webpack'],
+        "*.svg": ["@svgr/webpack"],
       },
     },
   },
-    images: {
-    domains: ['res.cloudinary.com'], // Add Cloudinary hostname here
+  images: {
+    domains: ["res.cloudinary.com"], // Add Cloudinary hostname here
   },
-  
+
+  experimental: {
+    serverComponentsExternalPackages: ["pg", "sequelize", "pg-hstore"],
+  },
+
+  // Important: Add this for Vercel
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [
+        ...(config.externals || []),
+        "pg",
+        "sequelize",
+        "pg-hstore",
+      ];
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
